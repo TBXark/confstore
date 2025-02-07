b61eaacbef9d01931364eec5c2da4b6211303a13
tbxark <tbxark@outlook.com>
2025-02-07 16:24:30 +0800

    feat: 添加CodecGroup类型及其序列化和反序列化方法

--- codec.go
+++ codec.go
@@ -1,6 +1,9 @@
 package confstore
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type Codec interface {
 	Marshal(value any) ([]byte, error)
@@ -15,4 +18,32 @@ func (JsonCodec) Marshal(value any) ([]byte, error) {
 
 func (JsonCodec) Unmarshal(data []byte, value any) error {
 	return json.Unmarshal(data, value)
+}
+
+type CodecGroup struct {
+	codecs []Codec
+}
+
+func NewCodecGroup(codecs ...Codec) *CodecGroup {
+	return &CodecGroup{codecs: codecs}
+}
+
+func (m *CodecGroup) Marshal(value any) ([]byte, error) {
+	for _, codec := range m.codecs {
+		data, err := codec.Marshal(value)
+		if err == nil {
+			return data, nil
+		}
+	}
+	return nil, fmt.Errorf("failed to marshal value")
+}
+
+func (m *CodecGroup) Unmarshal(data []byte, value any) error {
+	for _, codec := range m.codecs {
+		err := codec.Unmarshal(data, value)
+		if err == nil {
+			return nil
+		}
+	}
+	return fmt.Errorf("failed to unmarshal data")
 }
